e301336d804772365e236ea91e688f2bc507cca5
Basia Gocłowska <basia@MacBook-Pro-Basia.local>
2023-08-04 16:22:22 +0200

    Dodanie nagłówka i stopki

--- app.js
+++ app.js
@@ -20,3 +20,7 @@ app.use(express.static("public"));
 app.get("/", function (request, response) {
     response.render("home");
   });
+
+  app.listen(3000, function () {
+    console.log("Server started on port 3000");
+  });
